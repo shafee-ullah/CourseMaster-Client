7b927363e5f6642952d0f7aba5960396d6b48339
A K M Shafee Ullah <187232344+shafee-ullah@users.noreply.github.com>
2025-12-05 20:50:09 +0600

    backend uploaded on vercel

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     port: 5174,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://course-master-wheat.vercel.app',
         changeOrigin: true,
         secure: false,
       }
